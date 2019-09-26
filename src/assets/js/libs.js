50a06f166dc2c80dbd1390e8dae5b27b6a60d626
MarianVytak <marianvytak@gmail.com>
2019-09-26 19:20:56 +0300

    Added Home-6

--- src/assets/js/libs.js
+++ src/assets/js/libs.js
@@ -1,2 +1,3 @@
 //= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/bootstrap/dist/js/bootstrap.bundle.min.js
 //= ../../../bower_components/slick-carousel/slick/slick.js
