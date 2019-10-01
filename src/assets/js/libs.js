94b0d18c93c350c2d29d7666f3a0e6e738e1e6e5
MarianVytak <marianvytak@gmail.com>
2019-10-01 15:28:30 +0300

    Home-10: добавив Fancybox.

--- src/assets/js/libs.js
+++ src/assets/js/libs.js
@@ -1,3 +1,4 @@
 //= ../../../bower_components/jquery/dist/jquery.js
 //= ../../../bower_components/bootstrap/dist/js/bootstrap.bundle.min.js
 //= ../../../bower_components/slick-carousel/slick/slick.js
+//= ../../../bower_components/fancybox/dist/jquery.fancybox.min.js
